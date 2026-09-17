564b0a86bd83daddff932e4f912d0f050f3d80b1
MuhammadAdrianEfrichan <adrianefrich@gmail.com>
2026-09-17 19:52:30 +0700

    progres visi dan misi

--- src/components/CompanyPreview/CompanyPreview.jsx
+++ src/components/CompanyPreview/CompanyPreview.jsx
@@ -2,8 +2,12 @@ import Button from "../../ui/Button"
 import image from "../../assets/company/image.png"
 import companyOverview from "../../assets/company/companyOverview.svg"
 import category from "../../assets/company/category.svg"
+import { useNavigate } from "react-router-dom"
 
 const CompanyPreview =()=>{
+    const navigate = useNavigate();
+
+
     return <div className="flex gap-45 px-28.75 mt-20 h-198.5 justify-center items-center">
         <img src={image} alt="" />
         <div className="w-208.25 flex flex-col gap-7 ">
@@ -11,7 +15,7 @@ const CompanyPreview =()=>{
         <h1 className="text-[56px] font-header w-208.25 tracking-[-4px] h-39 font-semibold leading-[93%]"><div className="text-primary">PT Altama Surya Anugerah</div> adalah distributor resmi merek Tekiro, RYU, dan Rexco di Indonesia.</h1>
         <p className="text-grey text-[24px] leading-[125%]">Kami menghadirkan perkakas, mesin, dan chemical berkualitas tinggi yang kokoh, inovatif, dan terpercaya. Dengan jaringan  distribusi luas, kami menjadi mitra bagi para profesional maupun individu dalam mendukung berbagai kebutuhan.</p>
         <img src={category} alt="" className="w-139.5 h-9.5"/>
-        <Button className="mt-7">Read More</Button>
+        <Button className="mt-7" onClick={()=>{navigate('/visi-misi')}} >Read More</Button>
         </div>
 
     </div>
--- src/pages/VisiMisi/VisiMisi.jsx
+++ src/pages/VisiMisi/VisiMisi.jsx
@@ -1,11 +1,13 @@
 import Footer from "../../components/Footer"
 import HeroVisiMisi from "../../components/HeroVisiMisi"
 import Navbar from "../../components/Navbar"
+import VisiDanMisi from "../../components/VisiDanMisi"
 
 const VisiMisi = ()=>{
     return <>
         <Navbar />
         <HeroVisiMisi />
+        <VisiDanMisi />
         <Footer />
     </>
 }
